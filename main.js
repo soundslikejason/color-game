5bab9b44dfeb4a845068cf04900b3ab8878ca63b
Jason Leong <soundslikejason@users.noreply.github.com>
2017-01-13 20:23:58 -0800

    change and randomize colors

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ var colors = [
 	"rgb(0, 0, 255)",
 	"rgb(255, 0, 255)",
 ]
-
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +23,24 @@ for (var i = 0; i < squares.length; i++) {
 		var clickedColor = this.style.background;
 		//compare color to pickedColor
 		if (clickedColor === pickedColor) {
-			alert("correct");
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
 		} else {
-			alert("wrong");
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	});
+}
+
+function changeColors(color) {
+	//loop through all squares
+	for (var i = 0; i < squares.length; i++) {
+		//change each color to match given color
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
